f1c03cefa1c6f8ac230c1a0a1c244625ed6a523a
romithalder <109339749+romithalder@users.noreply.github.com>
2022-11-26 11:54:00 +0530

    Add files via upload

---
+++ EurekaServiceServer/EurekaServiceServer/EurekaServiceServer/src/test/java/com/EServer/EurekaServiceServer/EurekaServiceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EServer.EurekaServiceServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
